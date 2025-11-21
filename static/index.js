ee6d2c1fd8ce139db2cf0b80db0f64d11cf5548c
divyaramesh4 <divyaramesh3105@gmail.com>
2025-11-21 17:39:22 +0530

    static Keyword

---
+++ static/index.js
@@ -0,0 +1,43 @@
+// class MathUtil{
+//     static PI = 3.14
+//     static getDiameter(radius){
+//         return 2*radius
+//     }
+
+//     static getCircumference(radius){
+//         return 2*this.PI*radius
+//     }
+//     static getArea(radius){
+//         return this.PI*radius*radius;
+//     }
+// }
+// console.log(MathUtil.PI)
+// console.log(MathUtil.getDiameter(7))
+// console.log(MathUtil.getCircumference(7))
+// console.log(MathUtil.getArea(10))
+
+class User{
+
+    static userCount = 0;
+
+    constructor(username){
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount(){
+        console.log(`There are ${User.userCount} users online`);
+    }
+    sayHello(){
+        console.log(`Hello, my username is ${this.username}`);
+    }
+}
+
+const user1 = new User("Spongebob");
+const user2 = new User("Patrick");
+const user3 = new User("Sandy");
+
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+User.getUserCount();
