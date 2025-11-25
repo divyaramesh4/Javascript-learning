94af0f4d65640f7ceea435011b3f6f9451152021
divyaramesh4 <divyaramesh3105@gmail.com>
2025-11-25 12:02:22 +0530

    closures

--- closures/index.js
+++ closures/index.js
@@ -4,3 +4,55 @@
 //                  Allow for private variables and state maintenance
 //                  Used frequently in JS frameworks: React, Vue, Angular
 
+// function Outer(){
+//     let message = "Hello"
+//     function inner(){
+//         console.log(message)
+//     }
+//     inner();
+// }
+// Outer();
+// function createCounter() {
+//     let count = 0;
+//     function increment() {
+//         count++;
+//         console.log(`Count increased to ${count}`)
+//     }
+//     function getCount(){
+
+//     }
+//     return { increment }
+// }
+// const counter = createCounter();
+// counter.increment();
+// counter.increment();
+// counter.increment();
+
+function createGame(){
+
+    let score = 0;
+
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points}pts`);
+    }
+    
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+    
+    function getScore(){
+        return score;
+    }
+
+    return {increaseScore, decreaseScore, getScore};
+}
+
+const game = createGame();
+
+game.increaseScore(5);
+game.increaseScore(6);
+game.decreaseScore(3);
+
+console.log(`The final score is ${game.getScore()}pts`);
